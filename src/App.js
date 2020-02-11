c00567efd7ab48e1f5e9003742aa0972e7c7622f
Ike <ikescode@gmail.com>
2020-02-11 11:21:31 -0800

    style header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './Heading.css'
 import Heading from './Heading'
 
 function App() {
   return (
     <div className="App">
-      <Heading title="Hello World" subtitle="Foo Bar" />
-      <Heading title="Hello World 2" subtitle="Foo Bar 2" />
+      <Heading />
     </div>
   );
 }
---
+++ src/Heading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Heading(props) {
+    return (
+      <div className="Heading">
+        <header>
+          <h1>SF Public Spaces</h1>
+        </header>
+      </div>
+    )
+  }
+
+export default Heading
